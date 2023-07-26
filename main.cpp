0d81165d59ce4275de895e63f2ee7286533c73e3
fayadTarek <140421363+fayadTarek@users.noreply.github.com>
2023-07-26 19:23:18 +0300

    crating a user switch sellector betwuin blue and red

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-int main(){
-    cout <<"hello world!"<<endl;
+
+enum Cases{bleu, red};
+
+int main (){
+    Cases c =bleu;
+    int g(0);
+    do
+    {
+        cout << "Enter a number beteuin 0 and 1 :";
+        cin >> g;  
+    } while (g < 0 || g > 1);
+    
+    c =(Cases) g;
+
+    if(c == bleu)
+        cout << "Hello it's blue!";
+    else
+     cout << "Hello it's red!";
     return 0;
 }
